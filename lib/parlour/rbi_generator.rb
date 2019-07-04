2aea22659718e4c09960f8ca8911063346dbda8c
Aaron Christiansen <aaronc20000@gmail.com>
2019-07-04 14:19:42 +0100

    Create RbiGenerator

--- lib/parlour/rbi_generator.rb
+++ lib/parlour/rbi_generator.rb
@@ -1,6 +1,20 @@
-# typed: strong
+# typed: true
 module Parlour
   class RbiGenerator
-    
+    sig { params(break_params: Integer, tab_size: Integer).void }
+    def initialize(break_params: 4, tab_size: 2)
+      @break_params = break_params
+      @tab_size = tab_size
+      @root = Namespace.new
+    end
+
+    sig { returns(Integer) }
+    attr_reader :break_params
+
+    sig { returns(Integer) }
+    attr_reader :tab_size
+
+    sig { returns(Namespace) }
+    attr_reader :root
   end
 end
