2ae1f035c7119b8cdb1d18151d428975ba1ce378
Aaron Christiansen <aaronc20000@gmail.com>
2019-07-04 14:55:21 +0100

    Flesh out RbiObject interface

--- lib/parlour/rbi_generator/rbi_object.rb
+++ lib/parlour/rbi_generator/rbi_object.rb
@@ -3,7 +3,17 @@ module Parlour
   class RbiGenerator
     module RbiObject
       extend T::Helpers
+      extend T::Sig
       interface!
+
+      sig do
+        abstract.params(
+          indent_level: Integer,
+          break_params: Integer,
+          tab_size: Integer
+        ).returns(String)
+      end
+      def generate_rbi(indent_level, break_params, tab_size); end
     end
   end
 end
