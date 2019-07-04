7f988a03d4c62644b07989e5400b9c4f2d767a6f
Aaron Christiansen <aaronc20000@gmail.com>
2019-07-04 20:54:57 +0100

    Add Parameter

---
+++ lib/parlour/rbi_generator/parameter.rb
@@ -0,0 +1,72 @@
+# typed: true
+module Parlour
+  class RbiGenerator
+    class Parameter
+      extend T::Sig
+
+      sig do
+        params(
+          name: String,
+          type: T.nilable(String),
+          default: T.nilable(String)
+        ).void
+      end
+      def initialize(name, type: nil, default: nil)
+        @name = name
+
+        prefix = /^(\*\*|\*|\&)?/.match(name)&.captures&.first || ''
+        @kind = PREFIXES.rassoc(prefix).first
+
+        @kind = :keyword if kind == :normal && name.end_with?(':')
+
+        @type = type
+        @default = default
+      end
+
+      sig { returns(String) }
+      attr_reader :name
+
+      sig { returns(String) }
+      def name_without_kind
+        return T.must(name[0..-2]) if kind == :keyword
+
+        prefix_match = /^(\*\*|\*|\&)?[a-zA-Z_]/.match(name)
+        raise 'unknown prefix' unless prefix_match
+        prefix = prefix_match.captures.first || ''
+        T.must(name[prefix.length..-1])
+      end
+
+      sig { returns(T.nilable(String)) }
+      attr_reader :type
+
+      sig { returns(T.nilable(String)) }
+      attr_reader :default
+
+      sig { returns(Symbol) }
+      attr_reader :kind
+
+      sig { returns(String) }
+      def to_def_param
+        if default.nil?
+          "#{name}"
+        elsif !default.nil? && kind == :keyword
+          "#{name} #{default}"
+        else
+          "#{name} = #{default}"
+        end
+      end
+
+      sig { returns(String) }
+      def to_sig_param
+        "#{name_without_kind}: #{type || 'T.untyped'}"
+      end
+
+      PREFIXES = {
+        normal: '',
+        splat: '*',
+        double_splat: '**',
+        block: '&'
+      }.freeze
+    end
+  end
+end
---
+++ spec/parameter_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe Parlour::RbiGenerator::Parameter do
+  def pa(*a)
+    described_class.new(*a)
+  end
+
+  it 'determines kinds properly' do
+    expect(pa('foo').kind).to eq :normal
+    expect(pa('*foo').kind).to eq :splat
+    expect(pa('**foo').kind).to eq :double_splat
+    expect(pa('&foo').kind).to eq :block
+    expect(pa('foo:').kind).to eq :keyword
+  end
+
+  it 'determines #name_without_kind properly' do
+    expect(pa('foo').name_without_kind).to eq 'foo'
+    expect(pa('*foo').name_without_kind).to eq 'foo'
+    expect(pa('**foo').name_without_kind).to eq 'foo'
+    expect(pa('&foo').name_without_kind).to eq 'foo'
+    expect(pa('foo:').name_without_kind).to eq 'foo'
+  end
+
+  it 'can generate definitions' do
+    expect(pa('foo').to_def_param).to eq 'foo'
+    expect(pa('*foo').to_def_param).to eq '*foo'
+    expect(pa('**foo').to_def_param).to eq '**foo'
+    expect(pa('&foo').to_def_param).to eq '&foo'
+    expect(pa('foo:').to_def_param).to eq 'foo:'
+
+    expect(pa('foo', default: '3').to_def_param).to eq 'foo = 3'
+    expect(pa('foo:', default: '3').to_def_param).to eq 'foo: 3'
+  end
+
+  it 'can generate signatures' do
+    expect(pa('foo').to_sig_param).to eq 'foo: T.untyped'
+    expect(pa('*foo').to_sig_param).to eq 'foo: T.untyped'
+    expect(pa('**foo').to_sig_param).to eq 'foo: T.untyped'
+    expect(pa('&foo').to_sig_param).to eq 'foo: T.untyped'
+    expect(pa('foo:').to_sig_param).to eq 'foo: T.untyped'
+
+    expect(pa('foo', type: 'Integer', default: '3').to_sig_param).to eq 'foo: Integer'
+  end
+end
