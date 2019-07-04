3963e30b67d889197031b75ba171d20d56da397f
Aaron Christiansen <aaronc20000@gmail.com>
2019-07-04 14:24:01 +0100

    Add RbiObject interface

--- lib/parlour.rb
+++ lib/parlour.rb
@@ -1,6 +1,7 @@
 # typed: strong
 require 'parlour/version'
 
+require 'parlour/rbi_generator/rbi_object'
 require 'parlour/rbi_generator/namespace'
 require 'parlour/rbi_generator'
 
---
+++ lib/parlour/rbi_generator/rbi_object.rb
@@ -0,0 +1,9 @@
+# typed: true
+module Parlour
+  class RbiGenerator
+    module RbiObject
+      extend T::Helpers
+      interface!
+    end
+  end
+end
