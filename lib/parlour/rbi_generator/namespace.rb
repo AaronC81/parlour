f8e388f6e40514b34b06cc93b631a2594597ac2d
Aaron Christiansen <aaronc20000@gmail.com>
2019-07-04 14:26:25 +0100

    Add Namespace methods

--- lib/parlour/rbi_generator/namespace.rb
+++ lib/parlour/rbi_generator/namespace.rb
@@ -1,8 +1,18 @@
-# typed: strong
+# typed: true
 module Parlour
   class RbiGenerator
     class Namespace
+      extend T::Sig
 
+      include RbiObject
+
+      sig { void }
+      def initialize
+        @children = []
+      end
+
+      sig { returns(T::Array[RbiObject]) }
+      attr_reader :children
     end
   end
 end
