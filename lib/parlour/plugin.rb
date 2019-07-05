2ee83fd9f96d1c66a4d4637dcc7cb9629399a8cc
Aaron Christiansen <aaronc20000@gmail.com>
2019-07-05 22:45:31 +0100

    Document Plugin

--- lib/parlour/plugin.rb
+++ lib/parlour/plugin.rb
@@ -1,5 +1,7 @@
 # typed: true
 module Parlour
+  # The base class for user-defined RBI generation plugins.
+  # @abstract
   class Plugin
     extend T::Sig
     extend T::Helpers
@@ -8,16 +10,24 @@ module Parlour
     @@registered_plugins = {}
 
     sig { returns(T::Hash[String, Plugin]) }
+    # Returns all registered plugins, as a hash of their paths to the {Plugin}
+    # instances themselves.
     def self.registered_plugins
       @@registered_plugins
     end
 
     sig { params(new_plugin: T.class_of(Plugin)).void }
+    # Called automatically by the Ruby interpreter when {Plugin} is subclassed.
+    # This registers the new subclass into {registered_plugins}.
+    # @param new_plugin The new plugin.
     def self.inherited(new_plugin)
       registered_plugins[T.must(new_plugin.name)] = new_plugin.new
     end
 
     sig { params(plugins: T::Array[Plugin], generator: RbiGenerator).void }
+    # Runs an array of plugins on a given generator instance.
+    # @param plugins An array of {Plugin} instances.
+    # @param generator The {RbiGenerator} to run the plugins on.
     def self.run_plugins(plugins, generator)
       plugins.each do |plugin|
         generator.current_plugin = plugin
@@ -26,6 +36,10 @@ module Parlour
     end
 
     sig { abstract.params(root: RbiGenerator::Namespace).void }
+    # Plugin subclasses should redefine this method and do their RBI generation
+    # inside it.
+    # @abstract
+    # @param root The root {RbiGenerator::Namespace}.
     def generate(root); end
   end
 end
