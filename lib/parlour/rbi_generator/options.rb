4c949b1de57c4b58de1c4fca9c2c0d1b02087418
Aaron Christiansen <aaronc20000@gmail.com>
2019-07-05 22:10:31 +0100

    Document Options

--- lib/parlour/rbi_generator/options.rb
+++ lib/parlour/rbi_generator/options.rb
@@ -1,22 +1,36 @@
 # typed: true
 module Parlour
   class RbiGenerator
+    # A set of immutable formatting options passed to all calls of
+    # {RbiObject#generate_rbi}.
     class Options
       extend T::Sig
 
       sig { params(break_params: Integer, tab_size: Integer).void }
+      # Creates a new set of formatting options.
+      # @param break_params If there are at least this many parameters in a 
+      #   Sorbet `sig`, then it is broken onto separate lines.
+      # @param tab_size The number of spaces to use per indent.
       def initialize(break_params:, tab_size:)
         @break_params = break_params
         @tab_size = tab_size
       end
       
       sig { returns(Integer) }
+      # If there are at least this many parameters in a  Sorbet `sig`, then it 
+      # is broken onto separate lines.
       attr_reader :break_params
 
       sig { returns(Integer) }
+      # The number of spaces to use per indent.      
       attr_reader :tab_size
 
       sig { params(level: Integer, str: String).returns(String) }
+      # Returns a string indented to the given indent level, according to the
+      # set {tab_size}.
+      # @param level The indent level, as an integer. 0 is totally unindented.
+      # @param str The string to indent.
+      # @return The indented string.
       def indented(level, str)
         " " * (level * tab_size) + str
       end
